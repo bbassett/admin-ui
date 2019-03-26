aedd0e2a0f9cc5fec34353490807e2d68b335f98
Brandon Bassett <bassett.brandon@gmail.com>
2019-03-25 23:55:25 -0600

    rearrange some things

--- client/src/App.js
+++ client/src/App.js
@@ -1,78 +1,30 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
-import Cookies from 'js-cookie';
+import Index from './index/Index'
+import Browse from './browse/Browse'
+import Login from './login/Login'
+
 require('dotenv').config()
 
 class App extends Component {
-  state = {resp: '{}', access_token: Cookies.get('access_token')}
-  async componentDidMount() {
-    console.log(this.state.access_token);
-    const url = `${process.env.REACT_APP_API_URL}${window.location.pathname}`
-    let headers = {}
-    if(this.state.access_token) {
-      headers = {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${this.state.access_token}`
-      }
-    } else {
-      headers = {
-        'Content-Type': 'application/json',
-      }
-    }
-    const response = await fetch(url, {headers: headers})
-    const resp   = await response.json()
-    this.setState({resp: JSON.stringify(resp)})
-  }
-
   render() {
     return (
-      <div>
-        <h2>{process.env.REACT_APP_API_URL}</h2>
-        { recursive_list(JSON.parse(this.state.resp)) }
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/login">Login</Link></li>
+              <li><Link to="/browse">Browse</Link></li>
+            </ul>
+          </nav>
+          <Route path="/" exact component={Index} />
+          <Route path="/browse/" component={Browse} />
+          <Route path="/login/" exact component={Login} />
+        </div>
+      </Router>
     );
-  }
-}
-
-function recursive_list(body) {
-  const keys = Object.keys(body)
-
-  return (
-    <ul>
-    {
-      keys.map(key => {
-        return key == "href"
-        ? <li>{ link_to(body[key]) }</li>
-        : <li>{ key }: { get_val(body[key]) }</li>
-      })
-    }
-    </ul>
- )
-}
-
-function link_to(url) {
-  const path = new URL(url).pathname;
-  return <a href={ path }>{ path }</a>
-}
-
-function get_val(value) {
-  if (value == null) {
-    return "null"
-  } else if (typeof(value) === "object") {
-    if(value.method && value.method === "POST") {
-      return "form"
-    } else {
-      return recursive_list(value)
-    }
-  } else if (typeof(value) === "boolean") {
-    if (value) {
-      return "true"
-    } else {
-      return "false"
-    }
-  } else {
-    console.log(typeof(value))
-    return value
   }
 }
 
---
+++ client/src/browse/Browse.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import './Browse.css';
+import Cookies from 'js-cookie';
+
+class Browse extends Component {
+  state = {jsonBody: '{}', access_token: Cookies.get('access_token')}
+  async componentDidMount() {
+    console.log(this.state.access_token);
+
+    const url = get_api_url(window.location.pathname)
+
+    let headers = {'Content-Type': 'application/json'}
+    if(this.state.access_token) {
+      headers['Authorization'] = `Bearer ${this.state.access_token}`
+    }
+
+    const api_response = await fetch(url, {headers: headers})
+    const jsonBody = await api_response.json()
+    this.setState({jsonBody: JSON.stringify(jsonBody)})
+
+    console.log(jsonBody)
+  }
+
+  render() {
+    return (
+      <div>
+        { recursive_list(JSON.parse(this.state.jsonBody)) }
+      </div>
+    );
+  }
+}
+
+function get_api_url(path) {
+  path = path.replace('/browse', '');
+  return `${process.env.REACT_APP_API_URL}${path}`
+}
+
+function recursive_list(body) {
+  const keys = Object.keys(body)
+
+  return (
+    <ul>
+    {
+      keys.map(key => {
+        return key === "href"
+        ? <li key={ key }>{ link_to(body[key]) }</li>
+        : <li key={ key }>{ key }: { get_val(body[key]) }</li>
+      })
+    }
+    </ul>
+ )
+}
+
+function link_to(url) {
+  const path = new URL(url).pathname;
+  return <a href={ `/browse${path}` }>{ path }</a>
+}
+
+function get_val(value) {
+  if (value == null) {
+    return "null"
+  } else if (typeof(value) === "object") {
+    if(value.method && value.method === "POST") {
+      return "form"
+    } else {
+      return recursive_list(value)
+    }
+  } else if (typeof(value) === "boolean") {
+    if (value) {
+      return "true"
+    } else {
+      return "false"
+    }
+  } else {
+    console.log(typeof(value))
+    return value
+  }
+}
+
+export default Browse;

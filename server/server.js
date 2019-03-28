0d9a872ec3532cf9f9f0738208626bb0e703abc4
Brandon Bassett <bassett.brandon@gmail.com>
2019-03-27 21:12:43 -0600

    log path

--- server/server.js
+++ server/server.js
@@ -45,6 +45,7 @@ app.use(express.static(path.join(__dirname, staticFile)));
 // any routes not picked up by the server api will be handled by the react router
 app.get('/*', (req, res) => {
   console.log('catch-all');
+  console.log(path.join(__dirname, staticFile));
   res.sendFile(path.join(__dirname, staticFile))
 });
 
